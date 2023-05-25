3fe046afb35281666976185820356c7177b74e96
Nirajchemjong <nirajlimbu7@gmail.com>
2023-05-25 16:18:12 +1000

    tasklist and addes useState

--- src/component/FormCom.js
+++ src/component/FormCom.js
@@ -1,47 +1,90 @@
-import React from 'react';
-import { TableList } from '../component/TableList';
+import React, { useState } from "react";
 
+import { TableList } from "../component/TableList";
 
 export const FormCom = () => {
+  // const TaskList = [];
+
+  const [taskList, setTaskList] = useState([]);
+  const [task, setTask] = useState("");
+  const [hr, setHr] = useState("");
+
+  const handleOnChange = (e) => {
+    if (e.target.ariaLabel === "task") {
+      const { value } = e.target;
+      setTask(value);
+      console.log(`this is from task ${value}`);
+      // console.log(e);
+    } else {
+      const { value } = e.target;
+      setHr(value);
+      console.log(`this is from Hr ${value}`);
+      // console.log(e);
+    }
+  };
+
+  //handleOnsubmit
+  const id = "1";
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    // const newTask = FormData(e);
+    // setTask(newTask.get("task"));
+    // setHr(newTask.get("hr"));
+
+    let taskObj = {
+      id,
+      task,
+      hr,
+      type: "Entry",
+    };
+
+    setTaskList([...taskList, taskObj]);
+
+    // console.log(taskObj);
+  };
+  console.log(taskList);
+
   return (
     <div>
-    <div>
-   <form action="">
-          <div
-            class="row g-2 mt-5 border rounded-4 p-3 py-5 bg-light shadow-lg"
-          >
-            <div class="col-md-7">
+      <div>
+        <form
+          action=''
+          onSubmit={handleOnSubmit}
+        >
+          <div className='row g-2 mt-5 border rounded-4 p-3 py-5 bg-light shadow-lg'>
+            <div className='col-md-7'>
               <input
-                name="task"
-                type="text"
-                class="form-control"
-                placeholder="Your tasks"
-                value="Coding"
+                name='task'
+                type='text'
+                className='form-control'
+                placeholder='Your tasks'
+                onChange={handleOnChange}
+                aria-label='task'
                 required
               />
             </div>
-            <div class="col-md-2">
+            <div className='col-md-2'>
               <input
-                name="hr"
-                type="number"
-                class="form-control"
-                placeholder="33"
-                value="9"
+                name='hr'
+                type='number'
+                className='form-control'
+                placeholder='33'
+                onChange={handleOnChange}
+                aria-label='hr'
                 required
               />
             </div>
-            <div class="col-md-3 d-grid">
-              <button class="btn btn-primary">
-                <i class="fa-solid fa-plus"></i>
+            <div className='col-md-3 d-grid'>
+              <button className='btn btn-primary'>
+                <i className='fa-solid fa-plus'></i>
                 Add New Task
               </button>
             </div>
           </div>
         </form>
         <TableList />
-
+      </div>
     </div>
-   
-    </div>
-  )
-}
+  );
+};
--- src/component/TableList.js
+++ src/component/TableList.js
@@ -1,31 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export const TableList = () => {
   return (
     <div>
-    <div class="row mt-5">
-          <div class="col-md">
-            <h3 class="text-center">Task Entry List</h3>
-            <hr />
-            <table class="table table-striped table-hover border">
-              <tbody id="entry"></tbody>
-            </table>
-          </div>
-
-    
-          <div class="col-md">
-            <h3 class="text-center">Bad List</h3>
-            <hr />
-            <table class="table table-striped table-hover border">
-              <tbody id="bad"></tbody>
-            </table>
-            <p>
-              Total hrs you could have saved = <span id="badTotal">0</span> hr
-            </p>
-          </div>
+      <div className='row mt-5'>
+        <div className='col-md'>
+          <h3 className='text-center'>Task Entry List</h3>
+          <hr />
+          <table className='table table-striped table-hover border'>
+            <tbody id='entry'></tbody>
+          </table>
         </div>
 
-
+        <div className='col-md'>
+          <h3 className='text-center'>Bad List</h3>
+          <hr />
+          <table className='table table-striped table-hover border'>
+            <tbody id='bad'></tbody>
+          </table>
+          <p>
+            Total hrs you could have saved = <span id='badTotal'>0</span> hr
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
