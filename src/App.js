54116e245ad73f72ecf539c551a872cb35795e68
Nirajchemjong <nirajlimbu7@gmail.com>
2023-05-25 21:59:59 +1000

    fix packages issues and fixed unique Id

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-
-import './App.css';
-import { FormCom } from './component/FormCom';
-import { TableList } from './component/TableList';
-
-
+import "./App.css";
+import { FormCom } from "./component/FormCom";
+import { TableList } from "./component/TableList";
 
 function App() {
   return (
- <div className="wrapper">
-  <div className="container">
-  <div className="row">
-          <div class="col mt-5 text-center">
+    <div className='wrapper'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col mt-5 text-center'>
             <h1>Not to do list</h1>
           </div>
-    </div>
+        </div>
 
-    <FormCom/>
-    <TableList/>
-    
-  </div>
-   </div>
-  )
+        <FormCom />
+        <TableList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/component/FormCom.js
+++ src/component/FormCom.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-
+import { v4 as uuidv4 } from "uuid";
 import { TableList } from "../component/TableList";
+// import { v4 as uuid } from "uuid";
 
 export const FormCom = () => {
   // const TaskList = [];
@@ -13,18 +14,17 @@ export const FormCom = () => {
     if (e.target.ariaLabel === "task") {
       const { value } = e.target;
       setTask(value);
-      console.log(`this is from task ${value}`);
+      // console.log(`this is from task ${value}`);
       // console.log(e);
     } else {
       const { value } = e.target;
       setHr(value);
-      console.log(`this is from Hr ${value}`);
+      // console.log(`this is from Hr ${value}`);
       // console.log(e);
     }
   };
 
   //handleOnsubmit
-  const id = "1";
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
@@ -32,13 +32,30 @@ export const FormCom = () => {
     // setTask(newTask.get("task"));
     // setHr(newTask.get("hr"));
 
+    // const pass = `qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM`;
+    // let ids = "";
+    // const charLength = 6;
+    // for (let i; i < charLength; i++) {
+    //   const ranNum = Math.round(Math.random() * (pass.length - 1));
+    //   ids += pass[ranNum];
+    //   // setId(+pass[ranNum]);
+    //   console.log(setId(ids));
+    // }
+
+    // creating unique Ids==============================
+
+    const uid = uuidv4();
+    // Slicing uid in 6 length
+    const fid = uid.slice(0, 6);
+
+    // console.log(setId(uid.slice(0, 6)));
+
     let taskObj = {
-      id,
+      id: fid,
       task,
       hr,
       type: "Entry",
     };
-
     setTaskList([...taskList, taskObj]);
 
     // console.log(taskObj);
@@ -48,10 +65,7 @@ export const FormCom = () => {
   return (
     <div>
       <div>
-        <form
-          action=''
-          onSubmit={handleOnSubmit}
-        >
+        <form action='' onSubmit={handleOnSubmit}>
           <div className='row g-2 mt-5 border rounded-4 p-3 py-5 bg-light shadow-lg'>
             <div className='col-md-7'>
               <input
